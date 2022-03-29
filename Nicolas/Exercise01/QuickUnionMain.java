bc3517529100faea52a9c96c5fd0f97714349b55
CR600K3D <nicolas.quilitzsch@gmx.de>
2022-03-29 13:03:52 +0200

    QUN

--- Nicolas/Exercise01/QuickUnionMain.java
+++ Nicolas/Exercise01/QuickUnionMain.java
@@ -3,20 +3,21 @@ public class QuickUnionMain {
 
         int N = 10;
         UnionFind unionFind = new UnionFind(N);
+        Stopwatch stopwatch = new Stopwatch();
 
         int p[] = {4,3,6,9,2,5,7,6,4};
         int q[] = {3,8,5,4,1,0,2,1,8};
 
-        StdOut.println("");
-
-        for (int i = 0; i < p.length; i++){
-            if(unionFind.connected(p[i], q[i])) StdOut.println("Schon verbunden: " + p[i]
-                    + " und " + q[i]);
-            else {
-                unionFind.union(p[i], q[i]);
-                StdOut.println("Jetzt durch Aufruf der Union Methode verbunden " + p[i]
-                        + " und " + q[i]);
-            }
-        }
+//        StdOut.println("");
+//
+//        for (int i = 0; i < p.length; i++){
+//            if(unionFind.connected(p[i], q[i])) StdOut.println("Schon verbunden: " + p[i]
+//                    + " und " + q[i]);
+//            else {
+//                unionFind.union(p[i], q[i]);
+//                StdOut.println("Jetzt durch Aufruf der Union Methode verbunden " + p[i]
+//                        + " und " + q[i]);
+//            }
+//        }
     }
 }
